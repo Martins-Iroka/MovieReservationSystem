d2e86ad2cb32673a16c2af3778cd55884a116a89
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-05-05 14:19:04 +0100

    add background job to delete expired refresh tokens
    - Inject `UserService` to handle token cleanup
    - Launch a coroutine to periodically delete expired refresh tokens every 24 hours
    - Subscribe to the `ApplicationStopping` event to gracefully cancel the background job during shutdown

--- src/main/kotlin/plugins/BackgroundJobs.kt
+++ src/main/kotlin/plugins/BackgroundJobs.kt
@@ -1,6 +1,23 @@
 package com.martdev.plugins
 
-import io.ktor.server.application.Application
+import com.martdev.features.auth.domain.service.UserService
+import io.ktor.server.application.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import org.koin.ktor.ext.inject
+import kotlin.time.Duration.Companion.hours
 
 fun Application.configureBackgroundJobs() {
+    val userService by inject<UserService>()
+    launch {
+        while (isActive) {
+            delay(24.hours)
+            userService.deleteExpiredRefreshToken()
+        }
+    }
+    monitor.subscribe(ApplicationStopping) {
+        coroutineContext[Job]?.cancel()
+    }
 }
